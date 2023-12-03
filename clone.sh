8eb5af3c3d5fa64ef6afed54c3fb43528d97cff1
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-12-03 22:04:47 +0530

    Update Backup Script

--- clone.sh
+++ clone.sh
@@ -1,4 +1,6 @@
 git config --global credential.helper store
 git config --global user.name "torque_reborn"
 git config --global user.email "torquereborn@gmail.com"
-git clone https://gitlab.com/torquereborn/whatsapp /data/data/com.termux/files/home/storage/shared/Android/media/com.whatsapp/
+cd /data/data/com.termux/files/home/storage/shared/Android/media/com.whatsapp/
+mkdir .Shared
+git clone https://gitlab.com/torquereborn/whatsapp WhatsApp
