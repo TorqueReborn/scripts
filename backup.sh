f2c3b0278134479aca7248b04e9a42f2459724f3
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-12-03 22:06:35 +0530

    Update Backup Script

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 cd /data/data/com.termux/files/home/storage/shared/Android/media/com.whatsapp
 rm -rf .git
-git config --global --add safe.directory /storage/emulated/0/Android/media/com.whatsapp
+git config --global --add safe.directory /storage/emulated/0/Android/media/com.whatsapp/WhatsApp
 git config --global init.defaultBranch main
 git config lfs.https://gitlab.com/torquereborn/whatsapp.git/info/lfs.locksverify true
 git init
