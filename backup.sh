77f6b530660d2860fbc0a60fbb32e99dd1a0ef6f
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-12-03 20:20:35 +0530

    Update backup script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 cd /data/data/com.termux/files/home/storage/shared/Android/media/com.whatsapp
 rm -rf .git
+git config --global --add safe.directory /storage/emulated/0/Android/media/com.whatsapp
 git init
 git add -A
 git commit -m "Backup"
