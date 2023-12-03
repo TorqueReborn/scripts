bc7797bd8c1432bf7fe9593d28251b97e6d79067
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-12-03 21:48:22 +0530

    Update Backup Script

--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
 cd /data/data/com.termux/files/home/storage/shared/Android/media/com.whatsapp
 rm -rf .git
 git config --global --add safe.directory /storage/emulated/0/Android/media/com.whatsapp
+git config --global init.defaultBranch main
+git config lfs.https://gitlab.com/torquereborn/whatsapp.git/info/lfs.locksverify true
 git init
 git lfs install
 git add -A
@@ -8,3 +10,4 @@ git commit -m "Backup"
 git remote add origin https://gitlab.com/torquereborn/whatsapp.git
 git branch -M main
 git lfs push origin main
+git push --force -u origin main
