5ac213d18ed2674fffb947f089356b05294c68bb
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-12-03 20:11:36 +0530

    Clone script

---
+++ clone.sh
@@ -0,0 +1,4 @@
+git config --global credential.helper store
+git config --global user.name "Ghost-Reborn"
+git config --global user.email "abhinandammanamkandy@gmail.com"
+git clone https://github.com/TorqueReborn/WhatsApp /data/data/com.termux/files/home/storage/shared/Android/media/com.whatsapp/
